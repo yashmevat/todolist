33176f9371fa5948ed1a682646f61c835a07c10b
yashmevat <145139616+yashmevat@users.noreply.github.com>
2024-08-10 21:27:44 +0530

    Update DOM.js

--- DOM.js
+++ DOM.js
@@ -11,7 +11,7 @@ var listcontentafterdelete = ""
 list.innerHTML = JSON.parse(localStorage.getItem('list'));
 myform.addEventListener("submit", function (e) {
     e.preventDefault()
-    if (`${document.getElementById("data").value.trim()}` !== "") {
+     if(`${document.getElementById("data").value.trim()}`!==""){
         listcontent = `<li>
         <div class="text">${document.getElementById("data").value}</div>
         <div class="buttons">
